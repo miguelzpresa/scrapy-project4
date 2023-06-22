8722658e127c75d9f2581322c2549831fb5a667e
miguelzpresa <mikezpresa@gmail.com>
2023-06-21 21:55:47 -0600

    upload

---
+++ project4/project4/spiders/araña1.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+import scrapy
+from urllib.parse import urljoin
+from scrapy_playwright.page import PageCoroutine
+
+class CIAFactbookSpider(scrapy.Spider):
+    name = 'araña1'
+    start_urls = ['https://www.cia.gov/the-world-factbook/countries/afghanistan/']
+
+    custom_settings = {
+        'DOWNLOADER_MIDDLEWARES': {
+            'scrapy_playwright.middlewares.PlaywrightRequestMiddleware': 610,
+        },
+        'PLAYWRIGHT_BROWSER': 'chromium',
+        'PLAYWRIGHT_LAUNCH_OPTIONS': {'headless': True},
+    }
+
+    async def parse(self, response):
+        country_urls = []
+        div_elem = response.xpath('//div[@class="col-lg-9"]')
+        
+        for div in div_elem:
+            country_link = div.xpath('.//a[@class="inline-link"]')
+            if country_link:
+                country = country_link.xpath('./@href').get()[30:]
+                country_url = urljoin(response.url, country)
+                country_urls.append(country_url)
+
+        print(country_urls)
+
+        async with PageCoroutine() as page:
+            await page.goto(response.url)
+            await page.click('//*[@id="index-content-section"]/div/div[2]/div[2]/div/div/span[3]')
